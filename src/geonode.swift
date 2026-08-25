e01d796c1d67444fa698464f6bb08b84b72ceb0c
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2026-08-25 18:44:21 +0300

    Update geonode.swift

--- src/geonode.swift
+++ src/geonode.swift
@@ -20,6 +20,14 @@ extension URLSession {
     }
 }
 
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+    case patch = "PATCH"
+}
+
 public class Geonode{
     private let api = "https://proxylist.geonode.com/api"
     private var headers: [String: String]
